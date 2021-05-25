8183c291ee939258795bf3091dadbe23bea35a55
Nikhil Ajjarapu <nikhil.ajjarapu@utexas.edu>
2021-05-25 14:40:10 -0700

    Updates

---
+++ precache-manifest.fbfe5b347f452b71db90c860c45d1447.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bcd141b3a71511371343adbb7cd8ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3c94eff7e5c0251fb18",
+    "url": "/static/css/main.d7604045.chunk.css"
+  },
+  {
+    "revision": "e580bf63dbf860083148",
+    "url": "/static/js/2.ba58908e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ba58908e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c94eff7e5c0251fb18",
+    "url": "/static/js/main.a485af8c.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8b74fea46a7574be5968d2a3dde0798a",
+    "url": "/static/media/nikhilajjarapu.8b74fea4.png"
+  },
+  {
+    "revision": "827a73aad88363e5ac3a815d675afb71",
+    "url": "/static/media/resume.827a73aa.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.084a54faa853dacc348132f1b0a2bcbf.js"
+  "/precache-manifest.fbfe5b347f452b71db90c860c45d1447.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
